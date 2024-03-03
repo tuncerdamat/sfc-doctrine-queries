ea4170b8f549c87a5945d31224e7f1898f628502
tuncerdamat <tuncer.damat@gmail.com>
2024-03-03 21:53:00 +0100

    WHERE IN

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -58,11 +58,13 @@ class CategoryRepository extends ServiceEntityRepository
      */
     public function search(string $term): array
     {
+        $termList = explode(' ', $term);
         $qb = $this->addOrderByCategoryName();
             
         return $this->addFortuneCookieJoinAndSelect($qb)
-            ->andWhere('category.name LIKE :searchTerm OR category.iconKey LIKE :searchTerm OR fortuneCookie.fortune LIKE :searchTerm')
+            ->andWhere('category.name LIKE :searchTerm OR category.name IN (:termList) OR category.iconKey LIKE :searchTerm OR fortuneCookie.fortune LIKE :searchTerm')
             ->setParameter('searchTerm', '%'.$term.'%')
+            ->setParameter('termList', $termList)
             ->getQuery()
             ->getResult();
     }
